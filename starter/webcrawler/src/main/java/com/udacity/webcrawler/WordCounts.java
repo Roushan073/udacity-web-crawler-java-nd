4241cccdc36faf526bdb5c0a795e9d7f73b67d7e
Roushan Kumar <roushan.kumar@sigmoidanalytics.com>
2021-01-15 09:02:10 +0530

    sorting word count map using stream and lambda api

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
@@ -4,40 +4,44 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Utility class that sorts the map of word counts.
- *
- * <p>TODO: Reimplement the sort() method using only the Stream API and lambdas and/or method
- *          references.
  */
 final class WordCounts {
 
   /**
    * Given an unsorted map of word counts, returns a new map whose word counts are sorted according
    * to the provided {@link WordCountComparator}, and includes only the top
-   * {@param popluarWordCount} words and counts.
-   *
-   * <p>TODO: Reimplement this method using only the Stream API and lambdas and/or method
-   *          references.
-   *
+   * {@param popluarWordCount} words and counts.   *
    * @param wordCounts       the unsorted map of word counts.
    * @param popularWordCount the number of popular words to include in the result map.
    * @return a map containing the top {@param popularWordCount} words and counts in the right order.
    */
   static Map<String, Integer> sort(Map<String, Integer> wordCounts, int popularWordCount) {
+    /*
+      PriorityQueue<Map.Entry<String, Integer>> sortedCounts =
+      new PriorityQueue<>(wordCounts.size(), new WordCountComparator());
 
-    // TODO: Reimplement this method using only the Stream API and lambdas and/or method references.
+      sortedCounts.addAll(wordCounts.entrySet());
 
-    PriorityQueue<Map.Entry<String, Integer>> sortedCounts =
-        new PriorityQueue<>(wordCounts.size(), new WordCountComparator());
-    sortedCounts.addAll(wordCounts.entrySet());
-    Map<String, Integer> topCounts = new LinkedHashMap<>();
-    for (int i = 0; i < Math.min(popularWordCount, wordCounts.size()); i++) {
-      Map.Entry<String, Integer> entry = sortedCounts.poll();
-      topCounts.put(entry.getKey(), entry.getValue());
-    }
-    return topCounts;
+      Map<String, Integer> topCounts = new LinkedHashMap<>();
+
+      for (int i = 0; i < Math.min(popularWordCount, wordCounts.size()); i++) {
+        Map.Entry<String, Integer> entry = sortedCounts.poll();
+        topCounts.put(entry.getKey(), entry.getValue());
+      }
+
+      return topCounts;
+     */
+
+    Stream<Map.Entry<String, Integer>> sortedWordCountStream = wordCounts.entrySet().stream()
+            .sorted(new WordCountComparator()).limit(popularWordCount);
+
+    // Converting to LinkedHashMap to maintain the order
+    return sortedWordCountStream.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (key, value) -> key, LinkedHashMap::new));
   }
 
   /**
