0495f14f47b51ea1c05701f6a395cafa11c0f687
Roushan Kumar <roushan.kumar@sigmoidanalytics.com>
2021-01-16 11:57:30 +0530

    Profiler Changes

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,11 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Objects;
 
 /**
@@ -12,19 +15,34 @@ import java.util.Objects;
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
+  private final Object invokedTargetObject;
+  private final ProfilingState methodProfilingState;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  ProfilingMethodInterceptor(Clock clock, Object targetObject, ProfilingState methodProfilingState) {
     this.clock = Objects.requireNonNull(clock);
+    this.invokedTargetObject = Objects.requireNonNull(targetObject);
+    this.methodProfilingState = Objects.requireNonNull(methodProfilingState);
   }
 
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    return null;
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    // If the method contains @Profiled annotation record the invoked method call duration using ProfileState
+    if (method.isAnnotationPresent(Profiled.class)) {
+      Instant methodCallStartTime = clock.instant();
+      Object invokedResultObject;
+      try {
+        invokedResultObject = method.invoke(invokedTargetObject, args);
+      } catch (InvocationTargetException e) {
+        throw e.getTargetException();
+      } catch (Exception e) {
+        throw e.getCause();
+      } finally {
+        methodProfilingState.record(invokedTargetObject.getClass(), method, Duration.between(methodCallStartTime, clock.instant()));
+      }
+      return invokedResultObject;
+    }
+
+    // If the method does not contains the @Profiled annotation invoke the method without recording method call duration
+    return method.invoke(invokedTargetObject, args);
   }
 }
